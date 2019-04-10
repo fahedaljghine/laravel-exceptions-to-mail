491bafb26c5f61c21ea6304419de37e15b41c6f3
Fahed Aljghine <fahedaljghine2014@gmail.com>
2019-04-11 02:29:36 +0300

    update config and composer

--- config/config.php
+++ config/config.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+	/*
+     * Add email address you want exceptions to be sent
+	 * Please don't set my email address !!!
+     */
     'mails' => [
-        'f.aljghine@syriatrust.sy',
-        'W.AlNabulsi@syriatrust.sy',
-        's.hijlawi@syriatrust.sy'
+        //'fahedaljghine2014@gmail.com',
     ],
 ];
